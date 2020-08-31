22f175a26e8e3e66fa17844f2f1e2b3b955ea732
blgarust <66481915+blgarust@users.noreply.github.com>
2020-09-01 01:20:26 +0800

    2
    2

--- js/theme.js
+++ js/theme.js
@@ -61,22 +61,22 @@
 
 		/* ------------------------------------------------------------------------ */
 		/* 音乐
-		/* ------------------------------------------------------------------------ */
-		// $("#jquery_jplayer_1").jPlayer({
-  //       ready: function() {
-  //         $(this).jPlayer("setMedia", {
-  //           mp3: "http://playrust.xyz/src/boat-shop-music-loop.mp3"
-  //         }).jPlayer("play");
-  //         var click = document.ontouchstart === undefined ? 'click' : 'touchstart';
-  //         var kickoff = function () {
-  //           $("#jquery_jplayer_1").jPlayer("play");
-  //           document.documentElement.removeEventListener(click, kickoff, true);
-  //         };
-  //         document.documentElement.addEventListener(click, kickoff, true);
-  //       },
-  //       swfPath: "/js",
-  //       loop: true,
-  //       });
+		/* ------------------------------------------------------------------------ 
+		$("#jquery_jplayer_1").jPlayer({
+        ready: function() {
+          $(this).jPlayer("setMedia", {
+            mp3: "http://playrust.xyz/src/boat-shop-music-loop.mp3"
+          }).jPlayer("play");
+          var click = document.ontouchstart === undefined ? 'click' : 'touchstart';
+          var kickoff = function () {
+            $("#jquery_jplayer_1").jPlayer("play");
+            document.documentElement.removeEventListener(click, kickoff, true);
+          };
+          document.documentElement.addEventListener(click, kickoff, true);
+        },
+        swfPath: "/js",
+        loop: true,
+        });*/
 
 
 		/* ------------------------------------------------------------------------ */
